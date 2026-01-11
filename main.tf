383338972c1fe3f19e946722d542bcfc8714d948
Daniel Allen <dan@aeternagroup.net>
2026-01-11 17:55:35 +0000

    Update to schema version v4.57.0

---
+++ main.tf
@@ -0,0 +1,14 @@
+resource "azurerm_private_dns_txt_record" "private_dns_txt_records" {
+  for_each = var.private_dns_txt_records
+
+  name                = each.value.name
+  resource_group_name = each.value.resource_group_name
+  ttl                 = each.value.ttl
+  zone_name           = each.value.zone_name
+  tags                = each.value.tags
+
+  record {
+    value = each.value.record.value
+  }
+}
+
---
+++ variables.tf
@@ -0,0 +1,26 @@
+variable "private_dns_txt_records" {
+  description = <<EOT
+Map of private_dns_txt_records, attributes below
+Required:
+    - name
+    - resource_group_name
+    - ttl
+    - zone_name
+    - record (block):
+        - value (required)
+Optional:
+    - tags
+EOT
+
+  type = map(object({
+    name                = string
+    resource_group_name = string
+    ttl                 = number
+    zone_name           = string
+    tags                = optional(map(string))
+    record = object({
+      value = string
+    })
+  }))
+}
+
