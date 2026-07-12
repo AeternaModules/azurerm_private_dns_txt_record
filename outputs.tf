50f52d787b6b4be7408dc8b1f3bb7a2ca1a2fc45
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:19:49 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,33 +1,29 @@
-output "private_dns_txt_records" {
-  description = "All private_dns_txt_record resources"
-  value       = azurerm_private_dns_txt_record.private_dns_txt_records
-}
 output "private_dns_txt_records_fqdn" {
-  description = "List of fqdn values across all private_dns_txt_records"
-  value       = [for k, v in azurerm_private_dns_txt_record.private_dns_txt_records : v.fqdn]
+  description = "Map of fqdn values across all private_dns_txt_records, keyed the same as var.private_dns_txt_records"
+  value       = { for k, v in azurerm_private_dns_txt_record.private_dns_txt_records : k => v.fqdn }
 }
 output "private_dns_txt_records_name" {
-  description = "List of name values across all private_dns_txt_records"
-  value       = [for k, v in azurerm_private_dns_txt_record.private_dns_txt_records : v.name]
+  description = "Map of name values across all private_dns_txt_records, keyed the same as var.private_dns_txt_records"
+  value       = { for k, v in azurerm_private_dns_txt_record.private_dns_txt_records : k => v.name }
 }
 output "private_dns_txt_records_record" {
-  description = "List of record values across all private_dns_txt_records"
-  value       = [for k, v in azurerm_private_dns_txt_record.private_dns_txt_records : v.record]
+  description = "Map of record values across all private_dns_txt_records, keyed the same as var.private_dns_txt_records"
+  value       = { for k, v in azurerm_private_dns_txt_record.private_dns_txt_records : k => v.record }
 }
 output "private_dns_txt_records_resource_group_name" {
-  description = "List of resource_group_name values across all private_dns_txt_records"
-  value       = [for k, v in azurerm_private_dns_txt_record.private_dns_txt_records : v.resource_group_name]
+  description = "Map of resource_group_name values across all private_dns_txt_records, keyed the same as var.private_dns_txt_records"
+  value       = { for k, v in azurerm_private_dns_txt_record.private_dns_txt_records : k => v.resource_group_name }
 }
 output "private_dns_txt_records_tags" {
-  description = "List of tags values across all private_dns_txt_records"
-  value       = [for k, v in azurerm_private_dns_txt_record.private_dns_txt_records : v.tags]
+  description = "Map of tags values across all private_dns_txt_records, keyed the same as var.private_dns_txt_records"
+  value       = { for k, v in azurerm_private_dns_txt_record.private_dns_txt_records : k => v.tags }
 }
 output "private_dns_txt_records_ttl" {
-  description = "List of ttl values across all private_dns_txt_records"
-  value       = [for k, v in azurerm_private_dns_txt_record.private_dns_txt_records : v.ttl]
+  description = "Map of ttl values across all private_dns_txt_records, keyed the same as var.private_dns_txt_records"
+  value       = { for k, v in azurerm_private_dns_txt_record.private_dns_txt_records : k => v.ttl }
 }
 output "private_dns_txt_records_zone_name" {
-  description = "List of zone_name values across all private_dns_txt_records"
-  value       = [for k, v in azurerm_private_dns_txt_record.private_dns_txt_records : v.zone_name]
+  description = "Map of zone_name values across all private_dns_txt_records, keyed the same as var.private_dns_txt_records"
+  value       = { for k, v in azurerm_private_dns_txt_record.private_dns_txt_records : k => v.zone_name }
 }
 
