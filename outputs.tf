fccdebc397a005a67939044e064f7aeaceeffaf8
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:04:18 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "private_dns_txt_records_id" {
+  description = "Map of id values across all private_dns_txt_records, keyed the same as var.private_dns_txt_records"
+  value       = { for k, v in azurerm_private_dns_txt_record.private_dns_txt_records : k => v.id }
+}
 output "private_dns_txt_records_fqdn" {
   description = "Map of fqdn values across all private_dns_txt_records, keyed the same as var.private_dns_txt_records"
   value       = { for k, v in azurerm_private_dns_txt_record.private_dns_txt_records : k => v.fqdn }
